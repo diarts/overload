dfee7c6f424d2cee3d9774a422ff9167e54bb1a3
konstantin <korobanov>
2020-10-26 17:46:58 +0300

    added codes to exceptions and text to base exceptions

--- overload/exception/base.py
+++ overload/exception/base.py
@@ -5,4 +5,22 @@ __all__ = (
 
 class OverloadException(Exception):
     """Base class of overload lib exception"""
-    pass
+    __slots__ = ()
+    _text = 'Base overload exception.'
+    _code = 0
+
+    def __init__(self, text_=None):
+        super().__init__(text_ or self._text)
+
+    def __repr__(self):
+        return f'<{self.code}> {self.__class__.__name__} class - {self._text}'
+
+    @property
+    def text(self):
+        """Description of exception."""
+        return self._text
+
+    @property
+    def code(self):
+        """Code of exception."""
+        return self._code
--- overload/exception/type.py
+++ overload/exception/type.py
@@ -1,22 +1,32 @@
+from typing import Any
+
 from .base import OverloadException
 
 __all__ = (
     'TypeException',
     'UnknownType',
+    'CustomTypeError',
+    'CustomTypeAlreadyExist',
+    'IndexValueError',
 )
 
 
 class TypeException(OverloadException):
     """Base overload lib exception for overload.type.type._TypeHandler
     exceptions."""
-    pass
+    __slots__ = ()
+
+    _text = 'Base exception of type module.'
+    _code = 100
 
 
 class UnknownType(TypeException):
     """Exception raise if overload.type.type._TypeHandler
     catch unknown type."""
+    __slots__ = ()
 
     _text = 'Unknown type was found: {type}'
+    _code = 101
 
     def __init__(self, type_: type):
         """
@@ -25,3 +35,48 @@ class UnknownType(TypeException):
 
         """
         super().__init__(self._text.format(type=type_))
+
+
+class CustomTypeError(TypeException):
+    """Exception raise if user try add not type key to __custom_types__ of
+    overload.type.type._TypeHandler."""
+    __slots__ = ()
+
+    _text = 'Key must be an type type, current key {type} is {type} type.'
+    _code = 102
+
+    def __init__(self, type_: Any):
+        """
+        Args:
+            type_ (Any): Transmitted type for add in __custom_types__.
+
+        """
+        super().__init__(self._text.format(type=type_))
+
+
+class CustomTypeAlreadyExist(TypeException):
+    """Exception raise if user try add custom type
+    which already exist in __custom_types__."""
+    __slots__ = ()
+
+    _text = 'Type {type} already exist with index={index}.'
+    _code = 103
+
+    def __init__(self, type_: type, index: int):
+        """
+        Args:
+            type_ (type): Type which user try add in __custom_types__.
+            index (int): Index of exist type.
+
+        """
+        super().__init__(self._text.format(type=type_, index=index))
+
+
+class IndexValueError(TypeException):
+    """Exception raise if user try add new custom type with wrong index value
+    to overload.type.type._TypeHandler."""
+    __slots__ = ()
+
+    _text = ('Index for new type must be integer more than 99 '
+             'and not be already is use.')
+    _code = 104
