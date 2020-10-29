d1d1737f61ab9195bbdd076fb65810da8038d566
konstantin <korobanov>
2020-10-29 17:20:14 +0300

    added tests for add new implementation to matrix logic

---
+++ tests/test_utils/data.py
@@ -0,0 +1,28 @@
+from overload.type.type import _Type
+from overload.exception.matrix import (
+    ImplementationError,
+)
+
+
+def add_implementation_obj_1():
+    pass
+
+
+def add_implementation_obj_2():
+    pass
+
+
+def add_implementation_obj_3():
+    pass
+
+
+add_implementations = [
+    # format: callable_, args, exception, row_count, column_count
+    (add_implementation_obj_1, {'1': _Type(int)}, None,
+     {'1': [_Type(int)]}, 1),
+    (add_implementation_obj_1, None, ImplementationError, None, None),
+    (add_implementation_obj_2, {'2': _Type(str)}, None,
+     {'1': [_Type(int), None], '2': [None, _Type(str)]}, 2),
+    (add_implementation_obj_3, {'1': _Type(str)}, None,
+     {'1': [_Type(int), None, _Type(str)], '2': [None, _Type(str), None]}, 3),
+]
